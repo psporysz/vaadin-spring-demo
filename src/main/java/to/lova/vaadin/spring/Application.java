3ddf64d2dafc83aca4fac4af62fc12e6ee00a1a7
Giovanni Lovato <giovanni.lovato@axians.it>
2018-08-30 16:40:47 +0200

    Use CommonsMultipartResolver

--- src/main/java/to/lova/vaadin/spring/Application.java
+++ src/main/java/to/lova/vaadin/spring/Application.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new CommonsMultipartResolver();
     }
 
     @Override
--- src/main/java/to/lova/vaadin/spring/Home.java
+++ src/main/java/to/lova/vaadin/spring/Home.java
@@ -1,14 +1,21 @@
 package to.lova.vaadin.spring;
 
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.upload.Receiver;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class Home extends Div {
 
     public Home() {
-        this.add(new H1("Home"));
+        Receiver receiver = new MemoryBuffer();
+        Upload upload = new Upload(receiver);
+        upload.addSucceededListener(
+                event -> this.add(new H4("Upload successful!")));
+        this.add(upload);
     }
 
 }
