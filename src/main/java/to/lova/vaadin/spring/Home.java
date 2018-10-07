13975aef45f2b09be7a64bec58978c86886d42cd
Giovanni Lovato <giovanni.lovato@axians.it>
2018-10-07 20:32:21 +0200

    Update Spring Boot to 2.1.0.M4 and Vaadin to 12.0.0.alpha2

--- src/main/java/to/lova/vaadin/spring/Home.java
+++ src/main/java/to/lova/vaadin/spring/Home.java
@@ -2,7 +2,8 @@ package to.lova.vaadin.spring;
 
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.router.Route;
@@ -14,9 +15,10 @@ public class Home extends Div {
         this.add(new H1("Home"));
         var receiver = new MemoryBuffer();
         var upload = new Upload(receiver);
+        var list = new UnorderedList();
         upload.addSucceededListener(
-                event -> this.add(new Paragraph("Uploaded!")));
-        this.add(upload);
+                event -> list.add(new ListItem("Uploaded!")));
+        this.add(upload, list);
     }
 
 }
